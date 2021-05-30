44ff062368e12cfbe8c29506387963b02f6c0cd1
wooseok <mmyu2090@gmail.com>
2021-05-31 00:40:22 +0900

    api updates

--- QReceiptAPI/app/api.py
+++ QReceiptAPI/app/api.py
@@ -1,13 +1,26 @@
 from fastapi import FastAPI, Body, Depends
-from app.model import PostSchema, UserSchema, UserLoginSchema
+from fastapi.middleware.cors import CORSMiddleware
+from app.model import PostSchema, UserSchema, UserLoginSchema, ReceiptSchema
 from app.auth.auth_handler import signJWT
 from app.auth.auth_bearer import JWTBearer
 
 
 posts = []  # DB 대신 쓰는 배열
 users = []  # DB 대신 쓰는 배열
+receipts = []  # DB 대신 쓰는 배열
 
 app = FastAPI()
+
+origins = ["*", "210.91.148.210", "localhost:3000",
+           "localhost:8000", "localhost:8080"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 def check_user(data: UserLoginSchema):
@@ -59,7 +72,35 @@ async def user_login(user: UserLoginSchema = Body(...)):
 @app.post("/posts", dependencies=[Depends(JWTBearer())], tags=["posts"])
 async def add_post(post: PostSchema) -> dict:
     post.id = len(posts) + 1
-    posts.append(post.dict())
+    posts.append(post.dict())  # 여기서는 데이터를 그저 배열에 저장할뿐 나중에 DB에 해쉬 해서 저장할것
     return {
         "data": "post added."
+    }
+
+
+@app.post("/receipt", dependencies=[Depends(JWTBearer())], tags=["receipt"])
+async def add_receipt(myreceipt: ReceiptSchema = Body(...)) -> dict:
+    # 여기서는 데이터를 그저 배열에 저장할뿐 나중에 DB에 해쉬 해서 저장할것
+    receipts.append(myreceipt.dict())
+    return {
+        "data": "receipt added."
+    }
+
+
+@app.get("/receipt", dependencies=[Depends(JWTBearer())], tags=["receipt"])
+async def get_receipt() -> dict:
+    return {
+        "data": receipts
+    }
+
+
+@app.get("/receipt/{orderID}", dependencies=[Depends(JWTBearer())], tags=["receipt"])
+async def get_single_receipt(orderID: int) -> dict:
+    for receipt in receipts:
+        if receipt["orderID"] == orderID:
+            return {
+                "data": receipt
+            }
+    return {
+        "data": "there is no recipt has \"orderID\" like that"
     }
--- QReceiptAPI/app/model.py
+++ QReceiptAPI/app/model.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel, Field, EmailStr
+from typing import List
 
 
 class PostSchema(BaseModel):
@@ -14,19 +15,30 @@ class PostSchema(BaseModel):
             }
         }
 
+
 class UserSchema(BaseModel):
-    fullname: str = Field(...)
     email: EmailStr = Field(...)
     password: str = Field(...)
+    fullname: str = Field(...)
+    address1: str = Field(...)
+    address2: str = Field(...)
+    phoneNumber: str = Field(...)
+    userCat: str = Field(...)
 
     class Config:
         schema_extra = {
             "example": {
-                "fullname": "wooseok jang",
                 "email": "mmyu2090@gmail.com",
-                "password": "SuperPowerfulPW"
+                "password": "SuperPowerfulPW",
+                "fullname": "wooseok jang",
+                "address1": "경북 구미시 대학로 17-9",
+                "address2": "프라임 탑 201호",
+                "phoneNumber": "010-0000-0000",
+                "userCat": "점주"
+
             }
         }
+
 
 class UserLoginSchema(BaseModel):
     email: EmailStr = Field(...)
@@ -37,5 +49,56 @@ class UserLoginSchema(BaseModel):
             "example": {
                 "email": "mmyu2090@gmail.com",
                 "password": "SuperPowerfulPW"
+            }
+        }
+
+
+class OrderMenuSchema(BaseModel):
+    menuId: int = Field(...)
+    menuName: str = Field(...)
+    quantity: int = Field(...)
+    price: int = Field(...)
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "menuId": 1,
+                "menuName": "생수",
+                "quantity": 4,
+                "price": 4000
+            }
+        }
+
+
+class ReceiptSchema(BaseModel):
+    orderDate: str = Field(...)
+    orderID: int = Field(...)
+    seller: str = Field(...)
+    sellerID: str = Field(...)
+    sellerHP: str = Field(...)
+    orderItem: List[OrderMenuSchema]
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "orderDate": "21/04/23",
+                "orderID": 1472583691,
+                "seller": "주단태",
+                "sellerID": "SELLERID",
+                "sellerHP": "010-0000-0000",
+                "orderItem": [
+                    {
+                        "menuId": 1,
+                        "menuName": "생수",
+                        "quantity": 4,
+                        "price": 4000
+                    },
+                    {
+                        "menuId": 2,
+                        "menuName": "주단태빌리지 피규어",
+                        "quantity": 1,
+                        "price": 150000
+                    }
+                ]
             }
         }
